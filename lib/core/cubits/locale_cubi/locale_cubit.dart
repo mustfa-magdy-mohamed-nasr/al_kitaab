c2f687136290e9186c20d0ced86f2d50a3414486
mustfa-magdy-mohamed-nasr <Mostfammaaggddyy@gmail.com>
2024-06-26 15:47:32 +0300

    add shared_preferences  in ThemeCubit & LocaleCubit

--- lib/core/cubits/locale_cubi/locale_cubit.dart
+++ lib/core/cubits/locale_cubi/locale_cubit.dart
@@ -1,22 +1,53 @@
-import 'dart:ui';
-
+import 'dart:ui'; // Needed for Locale
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-// ignore: constant_identifier_names
+// Enum to define the themes available in the app
 enum AppTheme { Light, Dark }
 
+// Cubit to manage the app theme state
 class ThemeCubit extends Cubit<AppTheme> {
-  ThemeCubit() : super(AppTheme.Light);
+  ThemeCubit() : super(AppTheme.Light) {
+    _loadTheme();
+  }
 
   void toggleTheme() {
-    emit(state == AppTheme.Light ? AppTheme.Dark : AppTheme.Light);
+    final newTheme = state == AppTheme.Light ? AppTheme.Dark : AppTheme.Light;
+    emit(newTheme);
+    _saveTheme(newTheme);
+  }
+
+  Future<void> _loadTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    final themeIndex = prefs.getInt('app_theme') ?? 0;
+    emit(AppTheme.values[themeIndex]);
+  }
+
+  Future<void> _saveTheme(AppTheme theme) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('app_theme', theme.index);
   }
 }
 
+// Cubit to manage the app locale state
 class LocaleCubit extends Cubit<Locale> {
-  LocaleCubit() : super(const Locale('ar'));
+  LocaleCubit() : super(const Locale('ar')) {
+    _loadLocale();
+  }
 
   void toggleLocale(Locale newLocale) {
     emit(newLocale);
+    _saveLocale(newLocale);
+  }
+
+  Future<void> _loadLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    final localeCode = prefs.getString('app_locale') ?? 'ar';
+    emit(Locale(localeCode));
+  }
+
+  Future<void> _saveLocale(Locale locale) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('app_locale', locale.languageCode);
   }
 }
